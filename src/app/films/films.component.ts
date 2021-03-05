cbea94c65897743d8a93fe6d4cc9eef2adf16b7c
DESKTOP-9C48B5P\olenka <paukoleno4ka@gmail.com>
2021-03-05 23:14:30 +0200

    local stor

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -1,31 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { IFilm } from '../shared/film';
 import { FilmsService } from '../shared/films.service';
-import { tap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-films',
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.css']
 })
 export class FilmsComponent implements OnInit {
-  filmName: string = 'Alice';
+  filmName: string = '';
   page: number = 1;
   films: IFilm[] = []
+  localStorFilm: string = '';
+  localStorPage: number = 0;
   constructor(public filmsService: FilmsService) { }
 
   ngOnInit(): void {
 
+    this.setNameLocalStor();
+    this.getFilmsLocalStor()
   }
+  //set film name with local storage
+  setNameLocalStor() {
+    this.localStorFilm = localStorage.getItem('Film name') || '';
+    if (this.localStorFilm) {
+      this.filmName = this.localStorFilm;
+      this.searchFilms()
+    }
+  }
+  getFilmsLocalStor() {
+    this.localStorPage = Number(localStorage.getItem('page') || '');
+    if (this.localStorFilm) {
+      while (this.page > this.localStorPage) {
+        this.getFilms();
+        this.page++
+      }
+    }
+  }
+  //method - click on button
   searchFilms() {
-    if(this.filmName!==''){
-      this.filmsService.getFilms(this.filmName, this.page)
+    if (this.filmName !== '') {
+      this.getFilms()
+    }
+  }
+  //get all films with url
+  getFilms() {
+    this.filmsService.getFilms(this.filmName, this.page)
       .subscribe(
         (data: any) => this.films.push(...data.Search)
       )
-    console.log('subscribe',this.films)
-    this.filmName = ''
+    localStorage.setItem('Film name', this.filmName)
+    console.log('subscribe', this.films)
+    this.filmName = '';
+
+
+  }
+  // click on button more
+  more() {
+    if (this.localStorFilm !== '') {
+      this.filmName = this.localStorFilm;
+      this.page++;
+      this.getFilms()
+      localStorage.setItem('Page', (this.page).toString())
     }
-    
   }
 
 }
